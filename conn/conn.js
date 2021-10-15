2979e3387d9a3ffbceac102b9c4ea551b919e6f4
Núbia Segóvia <rsegovia.nubia@gmail.com>
2021-10-14 22:17:49 -0300

    [ADD] mongodb on localhost connected

---
+++ conn/conn.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Conn = () => {
+    mongoose.connect('mongodb://localhost:27017/tarefas', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('MongoDB conectado!'))
+    .catch((err) => console.log('Erro no mongo', err))
+}
+
+
+module.exports = Conn;
--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const TarefasRoutes = require('./routes/tarefas.routes')
+const TarefasRoutes = require('./routes/tarefas.routes');
+const Conn = require('./conn/conn');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+Conn();
+
 app.use('/tarefas', TarefasRoutes); //indica para o express utilizar as rotas no endpoin /vagas
 
 
+
 const port = 3000;
-
-
 
 app.listen(port, () => console.log(`App rodando em https://localhost:${port}/`));
 
